5c62b186ad42dfe00fe6ef2912c16477bb3d88a8
root <root@snaximus.cloud>
2026-06-16 18:02:16 +0000

    minor fix

--- managed/CounterStrikeSharp.API/Core/Application.cs
+++ managed/CounterStrikeSharp.API/Core/Application.cs
@@ -133,7 +133,9 @@ namespace CounterStrikeSharp.API.Core
             var versionString = $"v{Api.GetVersion()} ({Api.GetVersionString()})";
 
             info.ReplyToCommand(
-                "  CounterStrikeSharp was created and is maintained by Michael \"roflmuffin\" Wilson.\n" +
+                "  CounterStrikeSharp Minimal is a custom production-focused fork by Snaximus CS.\n" +
+                "  Based on CounterStrikeSharp, originally created by Michael \"roflmuffin\" Wilson.\n" +
+                "  This is not an official upstream CounterStrikeSharp release.\n" +
                 "  Counter-Strike Sharp uses code borrowed from SourceMod, Source.Python, FiveM, Saul Rennison, source2gen and CS2Fixes.\n" +
                 "  See ACKNOWLEDGEMENTS.md for more information.\n" +
                 "  Current API Version: " + versionString);
